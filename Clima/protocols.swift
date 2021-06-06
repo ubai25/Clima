eac7901dbdcbede4786111f7d2e03a8cc6449651
ubai25 <a.ubai25@gmail.com>
2021-06-06 10:15:02 +0700

    Initial Commit

---
+++ Clima/protocols.swift
@@ -0,0 +1,13 @@
+//
+//  protocols.swift
+//  Clima
+//
+//  Created by Ahmad Ubaidillah on 26/05/21.
+//  Copyright © 2021 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+protocol ChangeCityDelegate{
+    func userEnteredANewCityName(city : String)
+}
---
+++ Podfile
@@ -0,0 +1,14 @@
+platform :ios, '12.1'
+
+target 'Clima' do
+
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Clima
+
+    pod 'SwiftyJSON'
+    pod 'Alamofire'
+    pod 'SVProgressHUD'
+
+end
